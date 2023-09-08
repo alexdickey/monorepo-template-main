016ea153a7c54479f6b6593d764fdae482c717fd
Alex Dickey <alexander.c.dickey@gmail.com>
2023-09-08 14:08:44 -0700

    added refactorings

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -1,3 +1,5 @@
+import abc import ABC, abstractMethod
+
 """
  Sample modified from CS5500, Mike Shah
 
@@ -5,14 +7,27 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+
+class Shape(ABC):
+    @abstractMethod
+    def area(self):
+        pass
+
+    @abstractMethod
+    def set_values(self):
+        pass
+
+class Rectangle(Shape):
     def __init__(self, width, height):
         self._width = width
         self._height = height
 
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        self._width = x
+        self._height = y
+    
+    def get_values(self):
+        return (self._width, self._height)
 
     def area(self):
         return self.width * self.height
